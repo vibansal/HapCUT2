37a94280d4e4b7277f4564da6d0cbb5db6330c09
Peter Edge <edge.peterj@gmail.com>
2016-03-02 11:23:33 -0800

    added bits of code for Hi-C h-trans modeling. Majority still needs to be written

---
+++ hapcut-src/generate_test_htrans_probs.py
@@ -0,0 +1,11 @@
+total_dist = 250000000
+
+with open("test_htrans_probs.txt",'w') as outfile:
+    for dist in range(0,total_dist,10000):
+
+        prob = dist/30000000
+
+        if prob > 0.35:
+            prob = 0.35
+
+        print("{}\t{}".format(dist, prob),file = outfile)
