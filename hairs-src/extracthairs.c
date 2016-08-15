3b6b8b3e4050b833d83da4c9ad370f4cb7c64302
Peter Edge <edge.peterj@gmail.com>
2016-08-15 15:49:41 -0700

    changed print options slightly

--- hairs-src/extracthairs.c
+++ hairs-src/extracthairs.c
@@ -71,8 +71,8 @@ int parse_bamfile_sorted(char* bamfile, HASHTABLE* ht, CHROMVARS* chromvars, VAR
 
 void print_options() {
     fprintf(stderr, "\n PROGRAM TO extract haplotype informative reads (HAIRS) from coordinate sorted BAM files \n\n");
-    fprintf(stderr, "./extractHAIRS [options] --bam reads.sorted.bam --VCF variants.VCF   > output.fragments \n\n");
-    fprintf(stderr, "=============== PROGRAM OPTIONS ======================================== \n\n");
+    fprintf(stderr, "./extractHAIRS [options] --bam reads.sorted.bam --VCF variants.VCF --out fragment_file \n\n");
+    fprintf(stderr, "Options:\n");
     fprintf(stderr, "--qvoffset <33/64> : quality value offset, 33/64 depending on how quality values were encoded, default is 33 \n");
     fprintf(stderr, "--mbq <INT> : minimum base quality to consider a base for haplotype fragment, default 13\n");
     fprintf(stderr, "--mmq <INT> : minimum read mapping quality to consider a read for phasing, default 20\n");
--- hapcut2-src/printhaplotypes.c
+++ hapcut2-src/printhaplotypes.c
@@ -17,7 +17,7 @@ void print_hapcut_options() {
     fprintf(stderr, "--verbose, --v <0/1>:               verbose mode: print extra information to stdout and stderr. default: 0\n");
 
     fprintf(stderr, "\nRead Technology Options:\n");
-    fprintf(stderr, "--hic <0/1> :                       increases accuracy on HiC data; models h-trans errors directly from the data. default: 0\n");
+    fprintf(stderr, "--hic <0/1> :                       increases accuracy on Hi-C data; models h-trans errors directly from the data. default: 0\n");
     fprintf(stderr, "--hic_htrans_file, --hf <FILENAME>  optional tab-delimited input file where second column specifies h-trans error probabilities for insert size bins 0-50Kb, 50Kb-100Kb, etc.\n");
     fprintf(stderr, "--qv_offset, --qo <33/48/64> :      quality value offset for base quality scores, default: 33 (use same value as for extracthairs)\n");
     fprintf(stderr, "--long_reads, --lr <0/1> :          reduces memory when phasing long read data with many SNPs per read. default: automatic.\n");
@@ -31,16 +31,16 @@ void print_hapcut_options() {
     fprintf(stderr, "--error_analysis_mode, --ea <0/1>:  print confidence scores to haplotype file but don't split blocks or prune. default: 0\n");
 
     fprintf(stderr, "\nAdvanced Options:\n");
-    fprintf(stderr, "--new_format, --nf <0/1>:           use new HiC fragment matrix file format (but don't do h-trans error modeling). default: 0\n");
+    fprintf(stderr, "--new_format, --nf <0/1>:           use new Hi-C fragment matrix file format (but don't do h-trans error modeling). default: 0\n");
     fprintf(stderr, "--max_iter, --mi <int> :            maximum number of global iterations. Preferable to tweak --converge option instead. default: 10000\n");
     fprintf(stderr, "--maxcut_iter, --mc <int> :         maximum number of max-likelihood-cut iterations. Preferable to tweak --converge option instead. default: 10000\n");
-    fprintf(stderr, "--htrans_read_lowbound, --hrl <int> with --HiC on, h-trans probability estimation will require this many matepairs per window. default: 500\n");
-    fprintf(stderr, "--htrans_max_window, --hmw <int>    with --HiC on, the insert-size window for h-trans probability estimation will not expand larger than this many basepairs. default: 4000000\n");
+    fprintf(stderr, "--htrans_read_lowbound, --hrl <int> with --hic on, h-trans probability estimation will require this many matepairs per window. default: 500\n");
+    fprintf(stderr, "--htrans_max_window, --hmw <int>    with --hic on, the insert-size window for h-trans probability estimation will not expand larger than this many basepairs. default: 4000000\n");
 
-    fprintf(stderr, "\n\nHiC-specific Notes:\n");
-    fprintf(stderr, "  (1) When running extractHAIRS, must use --HiC 1 option to create a fragment matrix in the new HiC format.\n");
-    fprintf(stderr, "  (2) When running HapCUT2, use --HiC 1 if h-trans probabilities are unknown. Use --HiC_htrans_file if they are known\n");
-    fprintf(stderr, "  (3) Using --HiC_htrans_file is faster than --HiC and may yield better results at low read coverage (>30x).\n");
+    fprintf(stderr, "\n\nHi-C-specific Notes:\n");
+    fprintf(stderr, "  (1) When running extractHAIRS, must use --hic 1 option to create a fragment matrix in the new Hi-C format.\n");
+    fprintf(stderr, "  (2) When running HapCUT2, use --hic 1 if h-trans probabilities are unknown. Use --hic_htrans_file if they are known\n");
+    fprintf(stderr, "  (3) Using --hic_htrans_file is faster than --hic and may yield better results at low read coverage (>30x).\n");
     fprintf(stderr, "  (4) Set --converge to a larger value if possible/reasonable.\n ");
     fprintf(stderr, "\n");
 
