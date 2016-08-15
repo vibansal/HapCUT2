bb0b728325951b98834768cc45a133df0dc4722e
Peter Edge <edge.peterj@gmail.com>
2016-08-15 16:47:37 -0700

    fixed --skip_prune option

--- hapcut2-src/printhaplotypes.c
+++ hapcut2-src/printhaplotypes.c
@@ -24,7 +24,7 @@ void print_hapcut_options() {
 
     fprintf(stderr, "\nHaplotype Post-Processing Options:\n");
     fprintf(stderr, "--threshold, --t <float>:           threshold for pruning low-confidence SNPs (closer to 1 prunes more, closer to 0.5 prunes less). default: 0.8\n");
-    fprintf(stderr, "--skip_prune, --sp <0/1>:         split blocks using simple likelihood score to reduce switch errors. default: 0\n");
+    fprintf(stderr, "--skip_prune, --sp <0/1>:           skip default likelihood pruning step (prune SNPs after the fact using column 11 of the output). default: 0\n");
     fprintf(stderr, "--split_blocks, --sb <0/1>:         split blocks using simple likelihood score to reduce switch errors. default: 0\n");
     fprintf(stderr, "--split_threshold, --st <float>:    threshold for splitting blocks (closer to 1 splits more, closer to 0.5 splits less). default: 0.8\n");
     fprintf(stderr, "--call_homozygous, --ch <0/1>:      call positions as homozygous if they appear to be false heterozygotes. default: 0\n");
